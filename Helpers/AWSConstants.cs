6c52e0f5510780e531d3ad2994bf568fe4606e99
s3817324 <54796727+RyanRiddiford@users.noreply.github.com>
2022-05-17 16:47:14 +1000

    Code is ready

--- Helpers/AWSConstants.cs
+++ Helpers/AWSConstants.cs
@@ -6,9 +6,9 @@
     /// </summary>
     public class AWSConstants
     {
-        public const string S3_BUCKET_URL = "https://myapp-a3-cloud.s3.ap-southeast-2.amazonaws.com/";
+        public const string S3_BUCKET_URL = "";
 
-        public const string AWS_BUCKET_NAME = "myapp-a3-cloud";
+        public const string AWS_BUCKET_NAME = "";
 
     }
 }
